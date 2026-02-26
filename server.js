24ec0fd102285f4dbedec7c83707ebd8e27db16f
rudra2405 <rudrahirdekar4@gmail.com>
2026-02-26 22:46:48 +0530

    Fix json-server module

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 const jsonServer = require("json-server");
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
-const middlewares = jsonServer.defaults();
+const middlewares = jsonServer.defaults({
+  static: "public", // No public folder needed
+});
 const port = process.env.PORT || 3000;
 
 server.use(middlewares);
 server.use(router);
 server.listen(port, () => {
-  console.log(`JSON Server is running on port ${port}`);
+  console.log("JSON Server is running on port", port);
 });
